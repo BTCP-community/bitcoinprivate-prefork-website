ed704a42eba0953b3bf580e1218735bcdd643bdf
Braydon Batungbacal <me@braydonb.com>
2017-12-30 20:17:58 -0800

    Fixed absolute paths Moved everything for development back to root and using docs directory for github site hosting instead.

--- cloudforge/index.js
+++ cloudforge/index.js
@@ -3,25 +3,24 @@ const CloudForge = require('cloudforge');
 // Directories are relative to root, as cloudforge is used from npm runnable commands.
 module.exports = new CloudForge({
   server: {
-    directory: '../',
+    directory: './docs',
     browser: 'Google Chrome',
     port: 8585,
   },
   html: {
     sourceDirectory: './src/views',
-    buildDirectory: '../',
+    buildDirectory: './docs',
   },
   sass: {
     sourceDirectory: './src/sass',
-    buildDirectory: '../css',
+    buildDirectory: './docs/css',
     includeSourceMap: true,
     outputStyle: 'nested',
   },
-  cleanIgnoreDirectories: ['../'],
   dependencies: [
-    ['./src/images', '../images'],
-    ['./src/javascript', '../javascript'],
-    ['./src/dependencies', '../dependencies'],
-    ['./src/favicons', '../favicons'],
+    ['./src/images', './docs/images'],
+    ['./src/javascript', './docs/javascript'],
+    ['./src/dependencies', './docs/dependencies'],
+    ['./src/favicons', './docs/favicons'],
   ],
 });
