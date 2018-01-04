27733a8a16eff233b304aaca7769147156586607
Braydon Batungbacal <me@braydonb.com>
2018-01-04 10:24:29 -0800

    Updated telegram link.

--- docs/javascript/Localizations.js
+++ docs/javascript/Localizations.js
@@ -94,7 +94,7 @@ const Localizations = {
     'es': 'Español',
     'fr': 'Français',
     'he': 'עִבְרִית',
-    'hu': 'magyar',
+    'hu': 'Magyar',
     'id': 'Bahasa Indonesia',
     'ms': 'Malay',
     'nl': 'Nederlands',
