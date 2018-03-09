b61d3a0030bf252fd3e091fb6530dfc2ce0db151
DREKLI <col@drekli.com>
2018-03-09 22:14:15 +0100

    Viewport fix | Styling : Menu
    - Bug on devices for over scolled menu
    - Test fix to disable touch (not great on iOS)
    
    - Menu styles requests to be inverted

--- src/javascript/Main.js
+++ src/javascript/Main.js
@@ -31,6 +31,7 @@ $(function() {
   }
 
   var slideout = new Slideout({
+    'touch': false,
     'panel': document.getElementById('panel'),
     'menu': document.getElementById('menu'),
     'padding': 256,
