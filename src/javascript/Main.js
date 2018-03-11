51c1ee2d032bc8414abf2e3b3adf6985ba8618be
DREKLI <col@drekli.com>
2018-03-11 11:21:52 +0100

    Touch controls : Re-add

--- src/javascript/Main.js
+++ src/javascript/Main.js
@@ -31,7 +31,6 @@ $(function() {
   }
 
   var slideout = new Slideout({
-    'touch': false,
     'panel': document.getElementById('panel'),
     'menu': document.getElementById('menu'),
     'padding': 256,
